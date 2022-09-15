31c1444b09887bc11304938146656a5da5246eaa
Derek Yang <dyang@hamilton.edu>
2022-09-15 02:15:33 -0400

    supabase test

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+require('dotenv').config()
+const { createClient } = require('@supabase/supabase-js')
+// Current Parameters are for testing only
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
 const express = require("express")
 const app = express()
 const cors = require('cors')
@@ -8,4 +16,6 @@ app.get("/api", (req, res) => {
     res.json("Academic Committee")
 })
 
+console.log(supabase)
+console.log(process.env)
 app.listen(8001, () => console.log("Started on local port 8001"))
