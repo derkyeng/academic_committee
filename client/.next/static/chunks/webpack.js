86c980db071f6b2d9d65c60a172940837a657e20
Derek Yang <dyang@hamilton.edu>
2022-10-05 15:09:06 -0400

    removed server folder

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f6e155ebfa5f556"; }
+/******/ 		__webpack_require__.h = function() { return "9bac67f734f6125a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
