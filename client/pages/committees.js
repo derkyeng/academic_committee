242ea72820b8f26b2dd42ae6ac87859a35f0a5e0
RikiBorders <ninjab02@hotmail.com>
2022-10-09 19:14:51 -0400

    sql queries

--- client/pages/committees.js
+++ client/pages/committees.js
@@ -31,6 +31,97 @@ function committees() {
     }
     setCommittees(committees_data)
   }
+  
+  const getCommitteeDisplayNames = async () => {
+    let { data: committees, error } = await supabase
+      .from('committees')
+      .select('display_name')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(committees)
+  }
+  
+  getCommitteeDisplayNames()
+  
+  const getCommitteeDescriptions = async () => {
+    let { data: committees, error } = await supabase
+      .from('committees')
+      .select('description')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(committees)
+  }
+  
+  getCommitteeDescriptions()
+  
+  const getCommitteeRequiredRanks = async () => {
+    let { data: committees, error } = await supabase
+      .from('committees')
+      .select('required_ranks')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(committees)
+  }
+  
+  getCommitteeRequiredRanks()
+  
+  const getProfileUsernames = async () => {
+    let { data: profiles, error } = await supabase
+      .from('profiles')
+      .select('username')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(profiles)
+  }
+  
+  getProfileUsernames()
+  
+  const getProfileRanks = async () => {
+    let { data: profiles, error } = await supabase
+      .from('profiles')
+      .select('rank')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(profiles)
+  }
+  
+  getProfileRanks()
+  
+  const getProfileIds = async () => {
+    let { data: profiles, error } = await supabase
+      .from('profiles')
+      .select('id')
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(profiles)
+  }
+  
+  getProfileIds()
+  
+  const getProfileWithId = async (profile_id) => {
+    let { data: profiles, error } = await supabase
+      .from('profiles')
+      .select('id')
+      .where(`id=${profile_id}`)
+    if (error) {
+      console.error(error)
+      return
+    }
+  }
+  
+  getProfileWithId()
 
   const insertData = async (name) => {
     let { data, error } = await supabase
