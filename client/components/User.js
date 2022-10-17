147b37adda33632dffc72390ea3ed595ce4bd50b
Derek Yang <dyang@hamilton.edu>
2022-10-17 15:09:45 -0400

    added profile pics to users

--- client/components/User.js
+++ client/components/User.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
 import styles from './User.module.css'
 import {
     Button,
+    Avatar
   } from "flowbite-react";
 
 function User({user}) {
+    const [profilePic, setProfilePic] = useState(null);
+
+    async function getProfile() {
+        const { data } = supabase.storage
+          .from("avatars")
+          .getPublicUrl(`avatars/${user.id}`);
+        console.log(data.publicUrl);
+        setProfilePic(data.publicUrl);
+    }
+
+    useEffect(() => {
+        getProfile()
+    }, []);
     return(
         <div className={styles.item}>
-          <p className={styles.profile_name}>{user.username}</p>
-          <p>{user.description}</p>
-          <Button
+            <Avatar img={profilePic} rounded={true} />
+            <p className={styles.profile_name}>{user.username}</p>
+            <p>{user.description}</p>
+            <Button
                 className="button primary block"
                 style={{marginLeft: "auto"}}
             >
-                Add to Ballot
+            Add to Ballot
             </Button>
         </div>
       );
