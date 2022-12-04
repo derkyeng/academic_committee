bbf38e59e43c685ac5ec1b3412f6bfce5fd2e412
derkyeng <dyang@hamilton.edu>
2022-12-04 06:10:50 -0500

    individual admin toggle added

--- client/components/User.js
+++ client/components/User.js
@@ -5,18 +5,17 @@ import styles from "./User.module.css";
 import { Card, Button, Avatar } from "flowbite-react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { GoTrueAdminApi } from "@supabase/supabase-js";
 
 function User({ user, interested = false, committee, vote = false }) {
     const [profilePic, setProfilePic] = useState(null);
+    const [admin, setAdmin] = useState(null);
     const router = useRouter();
 
     async function getProfilePic() {
         console.log(user);
-        let { data: faculty_profiles, error } = await supabase
-            .from("faculty_profiles")
-            .select("profiles_id")
-            .eq("employeeID", user.employeeID);
-        const id = faculty_profiles[0].profiles_id;
+        setAdmin(user.admin);
+        const id = user.profiles_id;
         const { data } = supabase.storage.from("avatars").getPublicUrl(`avatars/${id}`);
         let isUndefined = data.publicUrl.substr(data.publicUrl.length - 9);
         if (isUndefined !== "undefined") {
@@ -28,6 +27,26 @@ function User({ user, interested = false, committee, vote = false }) {
             );
         }
     }
+
+    const makeAdmin = async () => {
+        let { data: status, error } = await supabase
+            .from("faculty_profiles")
+            .select("admin")
+            .eq("employeeID", user.employeeID);
+        console.log(status[0].admin);
+        setAdmin(!status[0].admin);
+        if (status[0].admin) {
+            const { data, error } = await supabase
+                .from("faculty_profiles")
+                .update({ admin: false })
+                .eq("employeeID", user.employeeID);
+        } else {
+            const { data, error } = await supabase
+                .from("faculty_profiles")
+                .update({ admin: true })
+                .eq("employeeID", user.employeeID);
+        }
+    };
 
     const addToBallot = async () => {
         console.log("add to ballot");
@@ -58,33 +77,40 @@ function User({ user, interested = false, committee, vote = false }) {
 
     return (
         <div className={styles.user}>
-            <Link href={"/faculty/" + user.employeeID}>
-                <Card className={styles.card}>
-                    <Avatar img={profilePic} rounded={true} />
-                    <p className={styles.profile_name}>
-                        {user.chosenfirstname} {user.chosenlastname}
-                    </p>
-                    <p>{user.title}</p>
-                    <p>{user.Department}</p>
-                    <div style={{ display: "flex", marginLeft: "auto" }}>
-                        <Button
-                            style={{ marginRight: "8px" }}
-                            className="button primary block"
-                            onClick={() => {
-                                router.push(`/faculty/${user.employeeID}`);
-                            }}
-                        >
-                            Visit Profile
+            <Card className={styles.card}>
+                <Avatar img={profilePic} rounded={true} />
+                <p className={styles.profile_name}>
+                    {user.chosenfirstname} {user.chosenlastname}
+                </p>
+                <p>{user.title}</p>
+                <p>{user.Department}</p>
+                <div style={{ display: "flex", marginLeft: "auto" }}>
+                    <Button
+                        style={{ marginRight: "8px" }}
+                        className="button primary block"
+                        onClick={() => {
+                            makeAdmin();
+                        }}
+                    >
+                        <div>{admin ? <p>Remove Admin Status</p> : <p>Give Admin Status</p>}</div>
+                    </Button>
+                    <Button
+                        style={{ marginRight: "8px" }}
+                        className="button primary block"
+                        onClick={() => {
+                            router.push(`/faculty/${user.employeeID}`);
+                        }}
+                    >
+                        Visit Profile
+                    </Button>
+                    {interested && (
+                        <Button className="button primary block" onClick={addToBallot}>
+                            Add to Ballot
                         </Button>
-                        {interested && (
-                            <Button className="button primary block" onClick={addToBallot}>
-                                Add to Ballot
-                            </Button>
-                        )}
-                        {vote && <Button className="button primary block">Vote</Button>}
-                    </div>
-                </Card>
-            </Link>
+                    )}
+                    {vote && <Button className="button primary block">Vote</Button>}
+                </div>
+            </Card>
         </div>
     );
 }
