36367e18a60589e31167ccd1cd7ba4df8325a005
Derek Yang <dyang@hamilton.edu>
2023-02-04 23:28:28 -0500

    Recreate (#36)
    * data passed from link component to page
    
    * committee pages set up

--- client/components/Committee.js
+++ client/components/Committee.js
@@ -5,7 +5,12 @@ import Link from "next/link";
 function Committee({ committee }) {
 	return (
 		<div>
-			<Link href={"/committees/" + committee.id}>
+			<Link
+				href={{
+					pathname: "/committees/" + committee.id,
+					query: committee,
+				}}
+			>
 				<Card>
 					<h1>{committee.display_name}</h1>
 					<h2>{committee.description}</h2>
