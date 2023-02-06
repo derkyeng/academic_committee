2f537a63df80bfa0c8d0dd359978d24bc4899675
Riki <ninjab02@hotmail.com>
2023-02-05 21:07:23 -0500

    Added Edit committee panel (still needs styling)

--- client/components/Committee.js
+++ client/components/Committee.js
@@ -1,21 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Card } from "flowbite-react";
+import { Button } from "flowbite-react";
+import styles from "./Committee.module.css";
+import EditCommitteeModal from "../components/CommitteesDisplay/EditCommitteeModal";
 import Link from "next/link";
 
 function Committee({ committee }) {
+	const [modal, setModal] = useState(false);
+
 	return (
 		<div>
-			<Link
+			{/* <Link
 				href={{
 					pathname: "/committees/" + committee.id,
 					query: committee,
 				}}
-			>
+			> */}
 				<Card>
 					<h1>{committee.display_name}</h1>
 					<h2>{committee.description}</h2>
+					<img 
+						src="/edit_icon.png" alt="edit button" 
+						className={styles.edit_button} 
+						onClick={() => setModal(true)}
+					/>
+ 		           <EditCommitteeModal closeModal={() => setModal(false)} modal={modal} committeeId={committee.id}/>
 				</Card>
-			</Link>
+			{/* </Link> */}
 		</div>
 	);
 }
