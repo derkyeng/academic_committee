5288cb84e679b7e3410908c045a940ac564af9e7
derkyeng <dyang@hamilton.edu>
2023-02-07 13:54:36 -0600

    changed login to not jump to index

--- client/hooks/index.js
+++ client/hooks/index.js
@@ -2,60 +2,58 @@ import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
 
 const useProfiles = () => {
-    const [profiles, setProfiles] = useState([]);
+	const [profiles, setProfiles] = useState([]);
 
-    async function searchForFaculty(
-        query_username = "",
-        query_rank = ""
-        // query_committees = [],
-        // query_interest_committee = [],
-    ) {
-        setProfiles([]);
-        // query_username = query_username.trim().toLowerCase();
+	async function searchForFaculty(
+		query_username = "",
+		query_rank = ""
+		// query_committees = [],
+		// query_interest_committee = [],
+	) {
+		setProfiles([]);
+		// query_username = query_username.trim().toLowerCase();
 
-        let query = supabase.from("faculty_profiles").select("*");
+		let query = supabase.from("profiles").select("*");
 
-        if (query_username) {
-            query = query.eq("username", query_username);
-        }
-        if (query_rank) {
-            query = query.eq("rank", query_rank);
-        }
-        // if (query_committee.length > 0) {
-        //     query = query.eq("committee", query_committee);
-        //} //committee is arr
+		if (query_username) {
+			query = query.eq("username", query_username);
+		}
+		if (query_rank) {
+			query = query.eq("rank", query_rank);
+		}
+		// if (query_committee.length > 0) {
+		//     query = query.eq("committee", query_committee);
+		//} //committee is arr
 
-        let { data: profiles_data, error } = await query;
+		let { data: profiles_data, error } = await query;
 
-        if (error) {
-            console.error(error);
-            return;
-        }
-        setProfiles(profiles_data);
-    }
+		if (error) {
+			console.error(error);
+			return;
+		}
+		setProfiles(profiles_data);
+	}
 
-    return [profiles, searchForFaculty];
+	return [profiles, searchForFaculty];
 };
 
 const useCommittees = () => {
-    const [committees, setCommittees] = useState([]);
+	const [committees, setCommittees] = useState([]);
 
-    const getCommittees = async () => {
-        let { data: committees_data, error } = await supabase
-            .from("committees")
-            .select("*");
-        if (error) {
-            console.error(error);
-            return;
-        }
-        setCommittees(committees_data);
-    };
+	const getCommittees = async () => {
+		let { data: committees_data, error } = await supabase.from("committees").select("*");
+		if (error) {
+			console.error(error);
+			return;
+		}
+		setCommittees(committees_data);
+	};
 
-    useEffect(() => {
-        getCommittees();
-    }, []);
+	useEffect(() => {
+		getCommittees();
+	}, []);
 
-    return [committees, getCommittees];
+	return [committees, getCommittees];
 };
 
 export { useProfiles, useCommittees };
--- client/pages/login.js
+++ client/pages/login.js
@@ -4,39 +4,39 @@ import { supabase } from "../utils/supabaseClient";
 import { TextInput, Label, Button } from "flowbite-react";
 
 const Login = () => {
-  const initialState = {
-    email: "",
-    password: "",
-  };
+	const initialState = {
+		email: "",
+		password: "",
+	};
 
-  const router = useRouter();
+	const router = useRouter();
 
-  return (
-    <div>
-      <div
-        className="h-screen flex items-center justify-center"
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          margin: "-25px -25px -25px -55px",
-        }}
-      >
-        <Button
-          onClick={async () => {
-            const { data, error } = await supabase.auth.signInWithOAuth({
-              provider: "google",
-            });
-            if (error) alert(error.message);
-            // push to home page
-            router.push("/account");
-          }}
-        >
-          Log In with Google
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div
+				className="h-screen flex items-center justify-center"
+				style={{
+					position: "absolute",
+					left: "50%",
+					top: "50%",
+					margin: "-25px -25px -25px -55px",
+				}}
+			>
+				<Button
+					onClick={async () => {
+						const { data, error } = await supabase.auth.signInWithOAuth({
+							provider: "google",
+						});
+						if (error) alert(error.message);
+						// push to home page
+						router.push("/");
+					}}
+				>
+					Log In with Google
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
