54bd853f476aa35cac775b1970460c63889db649
derkyeng <dyang@hamilton.edu>
2023-02-07 02:24:51 -0600

    changed index

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,36 +8,28 @@ import { Card } from "flowbite-react";
 import AccountView from "../components/Account";
 
 export default function Home({ session }) {
+	const getProfileWithEmail = async (email) => {
+		let { data: profiles, error } = await supabase
+			.from("faculty_profiles")
+			.select()
+			.eq("email", email);
+		if (error) {
+			console.error(error);
+			return;
+		}
+		console.log(profiles);
+		if (profiles.length == 0) {
+			console.log("profile does not exist");
+		} else {
+			console.log(profiles);
+		}
+	};
 
+	useEffect(() => {
+		getProfileWithEmail(session?.user.email);
+	}, [session]);
 
-  const getProfileWithEmail = async (email) => {
-    let { data: profiles, error } = await supabase
-      .from('faculty_profiles')
-      .select()
-      .eq('email', email)
-    if (error) {
-      console.error(error)
-      return
-    }
-    console.log(profiles)
-    if (profiles.length == 0){ 
-      console.log("profile does not exist")
-    } else {
-      console.log(profiles)
-    }
-  }
-
-  useEffect(() => {
-    getProfileWithEmail(session?.user.email)
-  }, [session]);
-
-  return (
-    <div style={{ padding: "50px 0 100px 0", margin: "15px" }}>
-      {session && (
-        <Card>
-          <AccountView session={session} />
-        </Card>
-      )}
-    </div>
-  );
+	return (
+		<div style={{ padding: "50px 0 100px 0", margin: "15px" }}>{session && <Card></Card>}</div>
+	);
 }
