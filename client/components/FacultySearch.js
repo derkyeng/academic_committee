2c2637bc277abb91c76770de58f630971d8ef388
derkyeng <dyang@hamilton.edu>
2023-02-18 01:54:41 -0600

    non query elements of search complete

--- client/components/FacultySearch.js
+++ client/components/FacultySearch.js
@@ -6,37 +6,37 @@ import FacultyForm from "./FacultySearch/FacultyForm";
 import { useProfiles } from "../hooks";
 
 export default function FacultySearch({ session }) {
-    const [formData, setFormData] = useState({
-        name: "",
-        committeeInterest: "",
-        rank: "",
-        committees: [],
-    });
+	const [formData, setFormData] = useState({
+		name: "",
+		committeeInterest: "",
+		level: "1",
+		rank: "",
+		committees: [],
+	});
 
-    const [profiles, searchForFaculty] = useProfiles();
+	const [profiles, searchForFaculty] = useProfiles();
 
-    return (
-        <div className={styles.faculty_search_comp}>
-            <Description>
-                Fill out the fields below In order to find faculty with specific
-                committee interests, tenure status, a particular name, or other
-                specifications.<br></br>
-                If you are not concerned with a particular field (i.e name does
-                not matter for your search), then leave the field as N/A.
-                Leaving <br></br> all fields as N/A will retrieve all faculty.
-            </Description>
+	return (
+		<div className={styles.faculty_search_comp}>
+			<Description>
+				Fill out the fields below In order to find faculty with specific committee
+				interests, tenure status, a particular name, or other specifications.<br></br>
+				If you are not concerned with a particular field (i.e name does not matter for your
+				search), then leave the field as N/A. Leaving <br></br> all fields as N/A will
+				retrieve all faculty.
+			</Description>
 
-            <FacultyForm
-                formData={formData}
-                handleFormChange={(newData) => setFormData(newData)}
-                handleFormSubmit={(filters) => searchForFaculty(filters)}
-            />
-            {profiles &&
-                profiles.map((user) => (
-                    <div className={styles.user_div} key={user.key}>
-                        <User user={user} key={user.id}></User>
-                    </div>
-                ))}
-        </div>
-    );
+			<FacultyForm
+				formData={formData}
+				handleFormChange={(newData) => setFormData(newData)}
+				handleFormSubmit={(filters) => searchForFaculty(filters)}
+			/>
+			{profiles &&
+				profiles.map((user) => (
+					<div className={styles.user_div} key={user.key}>
+						<User user={user} key={user.id}></User>
+					</div>
+				))}
+		</div>
+	);
 }
--- client/components/FacultySearch/FacultyForm.js
+++ client/components/FacultySearch/FacultyForm.js
@@ -5,41 +5,34 @@ import InterestedCommitteeSelect from "./InterestCommitteeSelect";
 import CommitteeSelect from "./CommitteeSelect";
 import NameTextInput from "./NameTextInput";
 import FacultySubmit from "./FacultySubmit";
+import InterestLevel from "./InterestLevel";
 
 export default function FacultyForm({
-    formData,
-    handleFormChange,
-    searchForFaculty,
-    handleFormSubmit,
+	formData,
+	handleFormChange,
+	searchForFaculty,
+	handleFormSubmit,
 }) {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const { name, rank, committees, committeeInterest } = formData;
-        handleFormSubmit(name, rank);
-    };
-    return (
-        <form onSubmit={handleSubmit} style={{ display: "flex" }}>
-            <RankSelect
-                formData={formData}
-                handleFormChange={handleFormChange}
-            />
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const { name, rank, level, committees, committeeInterest } = formData;
+		console.log("FORM DATA");
+		console.log(formData);
+		handleFormSubmit(name, rank, level);
+	};
+	return (
+		<form onSubmit={handleSubmit} style={{ display: "flex" }}>
+			<RankSelect formData={formData} handleFormChange={handleFormChange} />
 
-            <CommitteeSelect
-                formData={formData}
-                handleFormChange={handleFormChange}
-            />
+			<CommitteeSelect formData={formData} handleFormChange={handleFormChange} />
 
-            <InterestedCommitteeSelect
-                formData={formData}
-                handleFormChange={handleFormChange}
-            />
+			<InterestedCommitteeSelect formData={formData} handleFormChange={handleFormChange} />
 
-            <NameTextInput
-                formData={formData}
-                handleFormChange={handleFormChange}
-            />
+			<InterestLevel formData={formData} handleFormChange={handleFormChange} />
 
-            <FacultySubmit />
-        </form>
-    );
+			<NameTextInput formData={formData} handleFormChange={handleFormChange} />
+
+			<FacultySubmit />
+		</form>
+	);
 }
