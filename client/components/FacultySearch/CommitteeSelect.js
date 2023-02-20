41d251a44d3d730647ac6178caa2cc6b64aad9ea
Derek Yang <dyang@hamilton.edu>
2023-02-20 04:39:24 -0500

    New search (#46)
    * non query elements of search complete
    
    * search is done for everything but rank, will get to rank later

--- client/components/FacultySearch/CommitteeSelect.js
+++ client/components/FacultySearch/CommitteeSelect.js
@@ -2,38 +2,39 @@ import FacultySelect from "./FacultySelect";
 import { useCommittees } from "../../hooks";
 
 export default function CommitteeSelect({
-    title = "Committees",
-    id = "committees",
-    formData,
-    handleFormChange,
+	title = "Committees",
+	id = "committees",
+	formData,
+	handleFormChange,
 }) {
-    const [committees, getCommittees] = useCommittees();
-    const committeeOptions = committees
-        .map((item) => {
-            return {
-                value: item.id,
-                label: item.display_name,
-            };
-        })
-        .sort((a, b) => {
-            const nameA = a.label.toUpperCase(); // ignore upper and lowercase
-            const nameB = b.label.toUpperCase(); // ignore upper and lowercase
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
+	const [committees, getCommittees] = useCommittees();
+	const committeeOptions = committees
+		.map((item) => {
+			return {
+				value: item.id,
+				label: item.display_name,
+			};
+		})
+		.sort((a, b) => {
+			const nameA = a.label.toUpperCase(); // ignore upper and lowercase
+			const nameB = b.label.toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
 
-            return 0;
-        });
-    return (
-        <FacultySelect
-            title={title}
-            id={id}
-            formData={formData}
-            handleFormChange={handleFormChange}
-            options={committeeOptions}
-        />
-    );
+			return 0;
+		});
+	let noValue = { value: "", label: "N/A" };
+	return (
+		<FacultySelect
+			title={title}
+			id={id}
+			formData={formData}
+			handleFormChange={handleFormChange}
+			options={[noValue, ...committeeOptions]}
+		/>
+	);
 }
