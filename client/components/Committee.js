aa2537b764b97b2e4bcb2a68d99a3a993c72c08b
Riki <ninjab02@hotmail.com>
2023-02-19 22:14:33 -0500

    Updated styling for edit committee modal Added elected/appointment distinction for Committees Added elected field to committees (in database) Added relevant committee information to edit modal

--- client/components/Committee.js
+++ client/components/Committee.js
@@ -20,12 +20,18 @@ function Committee({ committee }) {
 				<Card>
 					<h1>{committee.display_name}</h1>
 					<h2>{committee.description}</h2>
+					<p>This committee is {committee.elected ? ("Elected") : ("Appointed")}</p>
 					<img 
 						src="/edit_icon.png" alt="edit button" 
 						className={styles.edit_button} 
 						onClick={() => setModal(true)}
 					/>
- 		           <EditCommitteeModal closeModal={() => setModal(false)} modal={modal} committeeId={committee.id}/>
+ 		           <EditCommitteeModal closeModal={() => setModal(false)} 
+						modal={modal} 
+						committeeId={committee.id} 
+						committeeName={committee.display_name} 
+						committeeElected={committee.elected}
+					/>
 				</Card>
 			{/* </Link> */}
 		</div>
