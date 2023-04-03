fff85d7eb3c4acc30627285dae9f10114a4cc296
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-04-03 00:57:16 -0400

    fix: fixed faculty profile bug and add label

--- client/pages/faculty/[id].js
+++ client/pages/faculty/[id].js
@@ -6,29 +6,34 @@ import { useAvatar } from "../../hooks";
 import { Avatar } from "flowbite-react";
 
 function id() {
-	const router = useRouter();
-	const user = router.query;
-	const [name, setName] = useState(null);
-	const [willingInterestedCommittees, setWillingInterestedCommittees] = useState([]);
-	const [interestedCommittees, setInterestedCommittees] = useState([]);
-	const [highInterestedCommittees, setHighInterestedCommittees] = useState([]);
-	const [currentCommittees, setCurrentCommittees] = useState([]);
-	const [pastCommittees, setPastCommittees] = useState([]);
-	const [profilePic] = useAvatar(user.id);
-	const [comment, setComment] = useState("");
+    const router = useRouter();
+    const user = router.query;
+    const [name, setName] = useState(null);
+    const [willingInterestedCommittees, setWillingInterestedCommittees] =
+        useState([]);
+    const [interestedCommittees, setInterestedCommittees] = useState([]);
+    const [highInterestedCommittees, setHighInterestedCommittees] = useState(
+        []
+    );
+    const [currentCommittees, setCurrentCommittees] = useState([]);
+    const [pastCommittees, setPastCommittees] = useState([]);
+    const [profilePic] = useAvatar(user.id);
+    const [comment, setComment] = useState("");
 
-	const getCommittees = async () => {
-		const user = router.query;
-		let { data: profiles, error } = await supabase
-			.from("profiles")
-			.select("username, interested_committees, current_committees, past_committees, comment")
-			.eq("id", user.id);
+    const getCommittees = async () => {
+        const user = router.query;
+        let { data: profiles, error } = await supabase
+            .from("profiles")
+            .select(
+                "username, interested_committees, current_committees, past_committees, comment"
+            )
+            .eq("id", user.id);
 
         console.log(profiles);
         if (error) {
             return;
         }
-
+        console.log(profiles[0]);
         let willing = [];
         let interested = [];
         let high = [];
@@ -77,72 +82,128 @@ function id() {
             past.push(committee);
         }
 
-		setWillingInterestedCommittees(willing);
-		setInterestedCommittees(interested);
-		setHighInterestedCommittees(high);
-		setCurrentCommittees(current);
-		setPastCommittees(past);
-		setComment(profiles[0].comment);
-		setName(profiles[0].username);
-	};
+        setWillingInterestedCommittees(willing);
+        setInterestedCommittees(interested);
+        setHighInterestedCommittees(high);
+        setCurrentCommittees(current);
+        setPastCommittees(past);
+        setComment(profiles[0].comment);
+        setName(profiles[0].username);
+    };
 
     const getCommitteeWithId = async (committeeId) => {
         let { data: committeeData, error } = await supabase
             .from("committees")
             .select("*")
             .eq("id", committeeId);
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return committeeData[0];
+    };
 
-	useEffect(() => {
-		getCommittees();
-		console.log(user);
-	}, [router]);
+    useEffect(() => {
+        getCommittees();
+        console.log(user);
+    }, [router]);
 
-	return (
-		<div className="w-full">
-			<h1 className="text-2xl underline font-bold mx-auto w-fit mt-8">{name}</h1>
-			<div className="w-full flex my-6 justify-center">
-				<Avatar img={profilePic} rounded={true} />
-			</div>
-			<div>
-				<h3 className="text-lg my-6 font-bold">Comments:</h3>
-				<div>{comment.length == 0 ? <p className="mt-6 mx-auto w-fit"></p> : <p>{comment}</p>}</div>
-			</div>
-			<div>
-				<h3 className="text-lg my-6 font-bold">Current Committees:</h3>
-				{currentCommittees.length == 0 ? (
-					<p className="mt-6 mx-auto w-fit"></p>
-				) : (
-					currentCommittees.map((committee_item) => <Committee committee={committee_item} key={committee_item.id} />)
-				)}
-			</div>
-			<div>
-				<h3 className="text-lg my-6 font-bold">Interested Committees:</h3>
-				{interestedCommittees.length == 0 ? (
-					<p className="mt-6 mx-auto w-fit"></p>
-				) : (
-					<div>
-						{willingInterestedCommittees.map((committee_item) => (
-							<Committee committee={committee_item} key={committee_item.id} />
-						))}
-						{interestedCommittees.map((committee_item) => (
-							<Committee committee={committee_item} key={committee_item.id} />
-						))}
-						{highInterestedCommittees.map((committee_item) => (
-							<Committee committee={committee_item} key={committee_item.id} />
-						))}
-					</div>
-				)}
-			</div>
-			<div>
-				<h3 className="text-lg my-6 font-bold">Past Committees:</h3>
-				{pastCommittees.length == 0 ? (
-					<p className="mt-6 mx-auto w-fit"></p>
-				) : (
-					pastCommittees.map((committee_item) => <Committee committee={committee_item} key={committee_item.id} />)
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="w-full">
+            <h1 className="text-2xl underline font-bold mx-auto w-fit mt-8">
+                {name}
+            </h1>
+            <div className="w-full flex my-6 justify-center">
+                <Avatar img={profilePic} rounded={true} />
+            </div>
+            <div>
+                <h3 className="text-lg my-6 font-bold">Comments:</h3>
+                <div>
+                    {comment.length == 0 ? (
+                        <p className="mt-6 mx-auto w-fit"></p>
+                    ) : (
+                        <p>{comment}</p>
+                    )}
+                </div>
+            </div>
+            <div>
+                <h3 className="text-lg my-6 font-bold">Current Committees:</h3>
+                {currentCommittees.length == 0 ? (
+                    <p className="mt-6 mx-auto w-fit"></p>
+                ) : (
+                    currentCommittees.map(
+                        (committee_item) =>
+                            committee_item && (
+                                <Committee
+                                    committee={committee_item}
+                                    key={committee_item.id}
+                                />
+                            )
+                    )
+                )}
+            </div>
+            <div>
+                <h3 className="text-lg my-6 font-bold">
+                    Interested Committees:
+                </h3>
+                {
+                    <div>
+                        <h3 className="text-lg font-bold">Willing to Serve:</h3>
+                        {willingInterestedCommittees.map(
+                            (committee_item) =>
+                                committee_item && (
+                                    <Committee
+                                        committee={committee_item}
+                                        key={committee_item.id}
+                                    />
+                                )
+                        )}
+                        <h3 className="text-lg font-bold">
+                            Interested in Serving:
+                        </h3>
+                        {interestedCommittees.map(
+                            (committee_item) =>
+                                committee_item && (
+                                    <Committee
+                                        committee={committee_item}
+                                        key={committee_item.id}
+                                    />
+                                )
+                        )}
+                        <h3 className="text-lg font-bold">
+                            High Interest in Serving:
+                        </h3>
+
+                        {highInterestedCommittees.map(
+                            (committee_item) =>
+                                committee_item && (
+                                    <Committee
+                                        committee={committee_item}
+                                        key={committee_item.id}
+                                    />
+                                )
+                        )}
+                    </div>
+                }
+            </div>
+            <div>
+                <h3 className="text-lg my-6 font-bold">Past Committees:</h3>
+                {pastCommittees.length == 0 ? (
+                    <p className="mt-6 mx-auto w-fit"></p>
+                ) : (
+                    pastCommittees.map(
+                        (committee_item) =>
+                            committee_item && (
+                                <Committee
+                                    committee={committee_item}
+                                    key={committee_item.id}
+                                />
+                            )
+                    )
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default id;
