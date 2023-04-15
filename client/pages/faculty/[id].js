fc54795fe4fb4ec2ea9d9bdf9b6e67941ea62d41
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-04-15 14:26:49 -0400

    fix merge bug

--- client/pages/faculty/[id].js
+++ client/pages/faculty/[id].js
@@ -10,19 +10,22 @@ function id() {
     const router = useRouter();
     const user = router.query;
     const [name, setName] = useState(null);
-    const [willingInterestedCommittees, setWillingInterestedCommittees] = useState([]);
+    const [willingInterestedCommittees, setWillingInterestedCommittees] =
+        useState([]);
     const [interestedCommittees, setInterestedCommittees] = useState([]);
-    const [highInterestedCommittees, setHighInterestedCommittees] = useState([]);
+    const [highInterestedCommittees, setHighInterestedCommittees] = useState(
+        []
+    );
     const [currentCommittees, setCurrentCommittees] = useState([]);
     const [pastCommittees, setPastCommittees] = useState([]);
     const [profilePic] = useAvatar(user.id);
     const [comment, setComment] = useState("");
     const [deptchair, setDeptChair] = useState(null);
-	const [leavestatus, setLeaveStatus] = useState(null);
-	const [deptChairMarked, setDeptChairMarked] = useState(null);
-	const [deptChairNotMarked, setDeptChairNotMarked] = useState(null);
-	const [leaveMarked, setLeaveMarked] = useState(null);
-	const [leaveNotMarked, setLeaveNotMarked] = useState(null);
+    const [leavestatus, setLeaveStatus] = useState(null);
+    const [deptChairMarked, setDeptChairMarked] = useState(null);
+    const [deptChairNotMarked, setDeptChairNotMarked] = useState(null);
+    const [leaveMarked, setLeaveMarked] = useState(null);
+    const [leaveNotMarked, setLeaveNotMarked] = useState(null);
     const [commentsModal, setCommentsModal] = useState(false);
 
     const getCommitteeDetails = async (uuids) => {
@@ -78,24 +81,22 @@ function id() {
         setPastCommittees(pastCommitteesList);
         setComment(userProfile.comment);
         setName(userProfile.username);
-    };
+
         setDeptChair(profiles[0].deptchair);
-        setLeaveStatus(profiles[0].leavestatus)
-        
+        setLeaveStatus(profiles[0].leavestatus);
+
         if (profiles[0].deptchair) {
             setDeptChairMarked(true);
-
         } else {
             setDeptChairNotMarked(true);
         }
 
         if (profiles[0].leavestatus) {
             setLeaveMarked(true);
-
         } else {
             setLeaveNotMarked(true);
         }
-	};
+    };
 
     const renderCommitteeItems = (committees) => {
         return committees.map((committee_item) => {
@@ -117,57 +118,72 @@ function id() {
         console.log(user);
     }, [router]);
 
-const getCommitteeWithId = async (committeeId) => {
-		let { data: committeeData, error } = await supabase.from("committees").select("*").eq("id", committeeId);
-		if (error) {
-			console.error(error);
-			return;
-		}
-		return committeeData[0];
-	};
+    const getCommitteeWithId = async (committeeId) => {
+        let { data: committeeData, error } = await supabase
+            .from("committees")
+            .select("*")
+            .eq("id", committeeId);
+        if (error) {
+            console.error(error);
+            return;
+        }
+        return committeeData[0];
+    };
 
     function ChairStatus() {
-		if (deptChairMarked) {
-			return (<div>
-				<p style={{ color: "#3399ff" }}>
-					<em>
-						You have indicated that you WILL be a department/program chair next year.
-					</em>
-				</p>
-			</div>)
-		}
-		return (<div>
-			<p style={{ color: "#cc0000" }}>
-				<em>
-					You have indicated that you will NOT be a department/program chair next year.
-				</em>
-			</p>
-		</div>)
-	}
+        if (deptChairMarked) {
+            return (
+                <div>
+                    <p style={{ color: "#3399ff" }}>
+                        <em>
+                            You have indicated that you WILL be a
+                            department/program chair next year.
+                        </em>
+                    </p>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <p style={{ color: "#cc0000" }}>
+                    <em>
+                        You have indicated that you will NOT be a
+                        department/program chair next year.
+                    </em>
+                </p>
+            </div>
+        );
+    }
 
-	function LeaveStatus() {
-		if (leaveMarked) {
-			return (<div>
-				<p style={{ color: "#3399ff" }}>
-					<em>
-						You have indicated that you WILL be on leave for one or both semesters next year.
-					</em>
-				</p>
-			</div>)
-		}
-		return (<div>
-			<p style={{ color: "#cc0000" }}>
-				<em>
-					You have indicated that you will NOT be on leave for one or both semesters next year.
-				</em>
-			</p>
-		</div>)
-	}
+    function LeaveStatus() {
+        if (leaveMarked) {
+            return (
+                <div>
+                    <p style={{ color: "#3399ff" }}>
+                        <em>
+                            You have indicated that you WILL be on leave for one
+                            or both semesters next year.
+                        </em>
+                    </p>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <p style={{ color: "#cc0000" }}>
+                    <em>
+                        You have indicated that you will NOT be on leave for one
+                        or both semesters next year.
+                    </em>
+                </p>
+            </div>
+        );
+    }
 
-	useEffect(() => {
-		getCommittees();
-		console.log(user);
-	}, [router]);
+    useEffect(() => {
+        getCommittees();
+        console.log(user);
+    }, [router]);
 
     return (
         <div className="w-full">
@@ -185,9 +201,13 @@ const getCommitteeWithId = async (committeeId) => {
                 </div>
             </div>
             <div>
-                <label style={{fontWeight: "bold"}}>Department Chair Status: {ChairStatus()}</label>
+                <label style={{ fontWeight: "bold" }}>
+                    Department Chair Status: {ChairStatus()}
+                </label>
 
-                <label style={{fontWeight: "bold"}}>Leave Status: {LeaveStatus()}</label>
+                <label style={{ fontWeight: "bold" }}>
+                    Leave Status: {LeaveStatus()}
+                </label>
             </div>
             <Modal
                 dismissible={true}
@@ -273,7 +293,6 @@ const getCommitteeWithId = async (committeeId) => {
             </CommitteePreviewWrapper>
         </div>
     );
-
 }
 
 export default id;
