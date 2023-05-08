b01ba2ce09642e9cfb64eeac580ac53bf11b0c6c
derkyeng <dyang@hamilton.edu>
2023-05-08 13:32:49 -0400

    admins can change admin status

--- client/components/User.js
+++ client/components/User.js
@@ -7,6 +7,28 @@ import { useAvatar } from "../hooks";
 
 function User({ user }) {
 	const [profilePic] = useAvatar(user.id);
+	const [admin, setAdmin] = useState(user.admin)
+	const makeAdmin = async () => {
+        let { data: status, error } = await supabase
+            .from("profiles")
+            .select("admin")
+            .eq("id", user.id);
+        console.log(status[0].admin);
+        if (status[0].admin) {
+            const { data, error } = await supabase
+                .from("profiles")
+                .update({ admin: false })
+                .eq("id", user.id);
+			setAdmin(false)
+        } else {
+            const { data, error } = await supabase
+                .from("profiles")
+                .update({ admin: true })
+                .eq("id", user.id);
+			setAdmin(true)
+        }
+    };
+
 	return (
 		<div className="cursor-pointer">
 			<Link
@@ -18,6 +40,19 @@ function User({ user }) {
 				<Card>
 					<Avatar img={profilePic} rounded={true} />
 					<h1>{user.username}</h1>
+					<div style={{ display: "flex", marginLeft: "auto" }}>
+						{!admin ? 
+							(<Button onClick={(e) => {
+								e.stopPropagation();
+								makeAdmin()
+								}}>
+								Give Admin Status</Button>) 
+							: 
+							(<Button onClick={(e) => {
+								e.stopPropagation();
+								makeAdmin()
+							}}>Remove Admin Status</Button>)}
+					</div>
 				</Card>
 			</Link>
 		</div>
