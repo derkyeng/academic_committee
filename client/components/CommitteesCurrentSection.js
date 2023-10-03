10f9fa2627aa2e2c41326d42f294e7d1115439c9
kitan23 <ktran@hamilton.edu>
2023-10-03 14:35:48 -0400

    code the current users section for committees

--- client/components/CommitteesCurrentSection.js
+++ client/components/CommitteesCurrentSection.js
@@ -1,13 +1,13 @@
 import User from "./User";
 
 const CurrentSection = ({ currentNames }) => {
-    return (
-        <div className="flex flex-wrap justify-center gap-4">
-            {currentNames.map((user, index) => {
-                return <User key={index} user={user} />;
-            })}
-        </div>
-    );
+	return (
+		<div className="flex my-6 flex-wrap justify-center gap-4">
+			{currentNames.map((user, index) => {
+				return <User key={index} user={user} />;
+			})}
+		</div>
+	);
 };
 
 export default CurrentSection;
--- client/components/CommitteesCurrentUsers.js
+++ client/components/CommitteesCurrentUsers.js
@@ -1,29 +1,29 @@
 import CurrentSection from "./CommitteesCurrentSection";
 
 function CommitteesCurrentUsers({ currentNames }) {
-    const isEmpty = currentNames === undefined || currentNames.length == 0;
+	const isEmpty = currentNames === undefined || currentNames.length == 0;
 
-    return (
-        <div className="mt-6 mx-20">
-            <h3 className="text-lg font-bold">Current Users:</h3>
+	return (
+		<div className="mt-6 mx-20">
+			<h3 className="text-lg font-bold">Current Users:</h3>
 
-            <div
-                style={{
-                    border: "solid",
-                    borderRadius: "10px",
-                    borderWidth: "2px",
-                    borderColor: "rgb(22, 45, 255)",
-                }}
-                className="my-6 flex flex-col items-center"
-            >
-                {isEmpty ? (
-                    <p className="my-6">No Current Members.</p>
-                ) : (
-                    <CurrentSection currentNames />
-                )}
-            </div>
-        </div>
-    );
+			<div
+				style={{
+					border: "solid",
+					borderRadius: "10px",
+					borderWidth: "2px",
+					borderColor: "rgb(22, 45, 255)",
+				}}
+				className="my-6 flex flex-col items-center"
+			>
+				{isEmpty ? (
+					<p className="my-6">No Current Members.</p>
+				) : (
+					<CurrentSection currentNames={currentNames} />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default CommitteesCurrentUsers;
