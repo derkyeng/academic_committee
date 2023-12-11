58a507846536d1563f1b7c206c0872fa037ce651
kitan23 <ktran@hamilton.edu>
2023-12-10 19:29:02 -0500

    no display on leave

--- client/components/CommitteesInterestSection.js
+++ client/components/CommitteesInterestSection.js
@@ -1,17 +1,22 @@
 import User from "./User";
 
 const InterestSection = ({ level, interestedNames, children }) => {
-    return (
-        <div className="my-6 flex flex-col items-center">
-            <h3 className="text-lg font-bold">{children}</h3>
-            <div className="flex flex-wrap justify-center gap-4">
-                {interestedNames[level] &&
-                    interestedNames[level].map((user, index) => {
-                        return <User key={index} user={user} />;
-                    })}
-            </div>
-        </div>
-    );
+	console.log("USERS", interestedNames[level]);
+	return (
+		<div className="my-6 flex flex-col items-center">
+			<h3 className="text-lg font-bold">{children}</h3>
+			<div className="flex flex-wrap justify-center gap-4">
+				{interestedNames[level] &&
+					interestedNames[level].map((user, index) => {
+						if (!user.leavestatus) {
+							return <User key={index} user={user} />;
+						} else {
+							return null;
+						}
+					})}
+			</div>
+		</div>
+	);
 };
 
 export default InterestSection;
--- client/components/SuccessMessage.js
+++ client/components/SuccessMessage.js
@@ -1,15 +1,18 @@
 const SuccessMessage = () => {
-    return (
-        <div
-            style={{
-                marginTop: "20px",
-                color: "#270",
-                backgroundColor: "#DFF2BF",
-            }}
-        >
-            <p>Your profile information has been updated!</p>
-        </div>
-    );
+	return (
+		<div
+			style={{
+				marginTop: "20px",
+				color: "#270",
+				backgroundColor: "#DFF2BF",
+			}}
+		>
+			<p>
+				Your profile information has been updated! Please refresh the
+				page to see new changes.
+			</p>
+		</div>
+	);
 };
 
 export default SuccessMessage;
